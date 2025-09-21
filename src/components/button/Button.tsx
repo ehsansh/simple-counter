acc5e50130839e993ec6c9a85d68ea3e48bb6cc4
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2025-09-21 20:21:41 +0330

    add aria labels to button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,13 +5,26 @@ interface Props {
     action: () => void;
     color?: 'primary' | 'danger' | 'secondary';
     isDisabled?: boolean;
+    'aria-label'?: string;
 }
 
-function Button({ children, action, color = 'primary', isDisabled = false }: Props) {
+function Button({
+    children,
+    action,
+    color = 'primary',
+    isDisabled = false,
+    'aria-label': ariaLabel,
+}: Props) {
     const className = [buttonBase, buttonColors[color], isDisabled ? buttonDisabled : ''].join(' ');
 
     return (
-        <button onClick={action} className={className} disabled={isDisabled}>
+        <button
+            onClick={action}
+            className={className}
+            disabled={isDisabled}
+            aria-disabled={isDisabled}
+            aria-label={ariaLabel}
+        >
             {children}
         </button>
     );
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -8,8 +8,15 @@ function Counter() {
         <div className="mt-8 flex flex-col items-center gap-4">
             <div className="text-2xl font-bold text-gray-800">Count is: {count}</div>
             <div className="flex gap-4">
-                <Button action={increment}>Increment</Button>
-                <Button isDisabled={count === 0} color="danger" action={decrement}>
+                <Button aria-label="Increment count" action={increment}>
+                    Increment
+                </Button>
+                <Button
+                    aria-label="Decrement count"
+                    isDisabled={count === 0}
+                    color="danger"
+                    action={decrement}
+                >
                     Decrement
                 </Button>
             </div>
